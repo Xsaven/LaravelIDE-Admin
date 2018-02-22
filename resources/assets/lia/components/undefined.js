1491f9e6eb422190b42eeccb58444739d9c9ca10
Xsaven <gms.sw.xsaven@gmail.com>
2018-02-22 21:27:29 +0200

    no src

--- resources/assets/lia/components/undefined.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(state){
-    return {
-        type:"line",
-        rows: [
-            { view:"template", type:"header", template:state.header },
-            { template:"<h1>Undefined template</h1>"},
-        ]
-    };
-}
--- resources/assets/lia/layout/idIncrement.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(componentName, uniqId){
-    return function(id){
-        return id+'['+componentName+']['+uniqId+']';
-    };
-};
