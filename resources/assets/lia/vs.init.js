2635bff8538448251eb7fd229e95ae96ff17aa3b
Xsaven <gms.sw.xsaven@gmail.com>
2018-03-18 00:06:45 +0200

    ide

--- resources/assets/lia/vs.init.js
+++ resources/assets/lia/vs.init.js
@@ -1,8 +1,2 @@
 require.config({ paths: { 'vs': 'vendor/lia/vs' }});
-require(['vs/editor/editor.main'], function() {
-    window.vs = monaco;
-    monaco.editor.create(document.getElementsByName("lookValue"), {
-        value: result,
-        language: "javascript"
-    });
-});
+require(['vs/editor/editor.main'], function(){});
